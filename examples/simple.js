b18c4ce83635be6b4e86ef13d505875b598600a9
Tjatse <thisnamemeansnothing@gmail.com>
2014-10-10 15:24:17 +0800

    example update

--- examples/simple.js
+++ examples/simple.js
@@ -1,15 +1,22 @@
+
 var read = require('../');
 
-read('http://amanpour.blogs.cnn.com/2014/05/08/defector-shadowy-organization-not-kim-jong-un-controls-north-korea/?hpt=hp_c1', function(err, art){
-  if(err){
-    console.log('[ERROR]', err.message);
-    return;
-  }
-  if(!art){
-    console.log('[WARNING] article not exist');
-    return;
-  }
+read('http://www.bbc.com/sport/0/football/29053651', {
+    timeout: 15000,
+    dataType: {
+        type: 'html',
+        stripSpaces: true
+    }
+}, function(err, art){
+    if(err){
+        console.log('[ERROR]', err.message);
+        return;
+    }
+    if(!art){
+        console.log('[WARNING] article not exist');
+        return;
+    }
 
-  console.log('[INFO]', 'content:', art.content);
-  console.log('[INFO]', 'title:', art.title);
+    console.log('[INFO]', 'title:', art.title);
+    console.log('[INFO]', 'content:', art.content);
 });
