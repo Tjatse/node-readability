c937afaebcf2b851954fb43653be06bda0cf6529
Tjatse <thisnamemeansnothing@gmail.com>
2015-01-03 11:50:20 +0800

    update example to latest

--- examples/simple.js
+++ examples/simple.js
@@ -1,23 +1,28 @@
-
 var read = require('../');
 
-read('http://baike.baidu.com/view/3974030.htm?fr=aladdin', {
-    timeout: 15000,
-    disableGzip: true,
-    output: {
-        type: 'html',
-        stripSpaces: true
+read('http://club.autohome.com.cn/bbs/thread-c-66-37239726-1.html', {
+  timeout  : 15000,
+  output   : {
+    type       : 'text',
+    stripSpaces: true
+  },
+  minTextLength: 0,
+  scoreRule: function(node){
+    if (node.hasClass('w740')) {
+      return 100;
     }
+    return 0;
+  }
 }, function(err, art){
-    if(err){
-        console.log('[ERROR]', err.message);
-        return;
-    }
-    if(!art){
-        console.log('[WARNING] article not exist');
-        return;
-    }
+  if (err) {
+    console.log('[ERROR]', err.message);
+    return;
+  }
+  if (!art) {
+    console.log('[WARNING] article not exist');
+    return;
+  }
 
-    console.log('[INFO]', 'title:', art.title);
-    console.log('[INFO]', 'content:', art.content);
+  console.log('[INFO]', 'title:', art.title);
+  console.log('[INFO]', 'content:', art.content);
 });
