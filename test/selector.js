8c440564f72281bf6232aa87b0ec0db653ccb703
Tjatse <thisnamemeansnothing@gmail.com>
2015-12-18 22:29:58 +0800

    refactor: extract data

--- test/selector.js
+++ test/selector.js
@@ -8,13 +8,13 @@ describe('custom selectors',function(){
   describe('should override default title and content selector',function(){
     it('if provided',function(done){
       read({
-        html: '<title>read-art</title><body><div class="article"><h3>Who Am I</h3><p class="section1">hi, dude, i am readability</p><p class="section2">aka read-art...</p><small class="author" data-author="Tjatse X">Tjatse</small></div></body>',
+        html: '<title>read-art</title><body><div class="article"><h3 title="--read-art--">Who Am I</h3><p class="section1">hi, dude, i am <b>readability</b></p><p class="section2">aka read-art...</p><small class="author" data-author="Tjatse X">Tjatse</small></div></body>',
         selectors: {
           title: {
             selector: '.article>h3',
             extract: ['text', 'title']
           },
-          content: '.article>p:nth-child(1)',
+          content: '.article p.section1',
           author: {
             selector: '.article>small.author',
             extract: {
@@ -22,12 +22,17 @@ describe('custom selectors',function(){
               full_name: 'data-author'
             }
           }
-        }
+        },
       }, function (err, art) {
         should.not.exist(err);
         expect(art).to.be.an('object');
-        art.content.should.contain('<a href="/Tjatse/read-art.git">');
-        art.title.should.equal('read-art');
+        expect(art.title).to.be.an('object');
+        art.title.title.should.equal('--read-art--');
+        art.title.text.should.equal('Who Am I');
+        art.content.should.not.contain('aka read-art');
+        expect(art.author).to.be.an('object');
+        art.author.shot_name.should.equal('Tjatse');
+        art.author.full_name.should.equal('Tjatse X');
         done();
       });
     });
