35c63e8d57c8829f14c25c88847293ec27b3e64f
HUANG Wei <grubbyfans@gmail.com>
2016-01-06 10:11:32 +0800

    Add a option imgFallback which use img element's data-src attribute when src attribute is missing.

--- test/links.js
+++ test/links.js
@@ -32,4 +32,47 @@ describe('fix links',function(){
     });
   });
 
+  describe('image fallback option',function(){
+    it('fallback to relative data-src if imgFallback option is true',function(done){
+      read({
+        uri: 'http://github.com/Tjatse',
+        html: '<title>read-art</title><body><div><p>hi, dude, i am <img data-src="/Tjatse/foo.png" />, aka read-art...</p></div></body>',
+        imgFallback: true,
+      }, function(err, art){
+        should.not.exist(err);
+        expect(art).to.be.an('object');
+        art.content.should.contain(' src="http://github.com/Tjatse/foo.png"');
+        art.title.should.equal('read-art');
+        done();
+      });
+    });
+
+    it('fallback to absolute data-src if imgFallback option is true',function(done){
+      read({
+        uri: 'http://github.com/Tjatse',
+        html: '<title>read-art</title><body><div><p>hi, dude, i am <img data-src="http://github.com/Tjatse/foo.png" />, aka read-art...</p></div></body>',
+        imgFallback: true,
+      }, function(err, art){
+        should.not.exist(err);
+        expect(art).to.be.an('object');
+        art.content.should.contain(' src="http://github.com/Tjatse/foo.png"');
+        art.title.should.equal('read-art');
+        done();
+      });
+    });
+
+    it('not fallback to data-src if imgFallback option is false',function(done){
+      read({
+        uri: 'http://github.com/Tjatse',
+        html: '<title>read-art</title><body><div><p>hi, dude, i am <img data-src="/Tjatse/foo.png" />, aka read-art...</p></div></body>',
+        imgFallback: false,
+      }, function(err, art){
+        should.not.exist(err);
+        expect(art).to.be.an('object');
+        art.content.should.not.contain('<img');
+        art.title.should.equal('read-art');
+        done();
+      });
+    });
+  });
 });
