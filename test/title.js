e2b84f69a90325a7d0479d56c8f00f7ccee68739
Tjatse <thisnamemeansnothing@gmail.com>
2016-02-22 18:26:55 +0800

    move title test case from option to title

--- test/title.js
+++ test/title.js
@@ -3,11 +3,11 @@ var read = require('../'),
   expect = chai.expect,
   should = chai.should();
 
-describe('find exact title',function(){
+describe('find exact title', function() {
 
-  describe('length less than 10 before separator',function(){
-    it('should remove separator',function(done){
-      read('<title>The | whole title including separator</title><body></body>', function(err, art){
+  describe('length less than 10 before separator', function() {
+    it('should remove separator', function(done) {
+      read('<title>The | whole title including separator</title><body></body>', function(err, art) {
         should.not.exist(err);
         expect(art).to.be.an('object');
         art.title.should.equal('The whole title including separator');
@@ -16,10 +16,10 @@ describe('find exact title',function(){
     });
   });
 
-  ['|', '-', '_', '«', '»'].forEach(function(s){
-    describe('better title',function(){
-      it('split with ' + s,function(done){
-        read('<title>Chapter of readability ' + s + ' Tjatse</title><body></body>', function(err, art){
+  ['|', '-', '_', '«', '»'].forEach(function(s) {
+    describe('better title', function() {
+      it('split with ' + s, function(done) {
+        read('<title>Chapter of readability ' + s + ' Tjatse</title><body></body>', function(err, art) {
           should.not.exist(err);
           expect(art).to.be.an('object');
           art.title.should.equal('Chapter of readability');
@@ -29,9 +29,9 @@ describe('find exact title',function(){
     });
   });
 
-  describe('title split with multi separators',function(){
-    it('should returns the first found length greater than 10',function(done){
-      read('<title>Chapter | Demonstration of readability - Tjatse</title><body></body>', function(err, art){
+  describe('title split with multi separators', function() {
+    it('should returns the first found length greater than 10', function(done) {
+      read('<title>Chapter | Demonstration of readability - Tjatse</title><body></body>', function(err, art) {
         should.not.exist(err);
         expect(art).to.be.an('object');
         art.title.should.equal('Chapter Demonstration of readability');
@@ -40,12 +40,43 @@ describe('find exact title',function(){
     });
   });
 
-  describe('better title',function(){
-    it('should return the first found',function(done){
-      read('<title>Chapter Demonstration of readability_Node.js_readability_GitHub</title><body></body>', function(err, art){
+  describe('better title', function() {
+    var article = '<title>Chapter Demonstration of readability_Node.js_readability_GitHub</title><body></body>';
+
+    it('by default', function(done) {
+
+      read({
+        html: article
+      }, function(err, art) {
         should.not.exist(err);
-        expect(art).to.be.an('object');
-        art.title.should.equal('Chapter Demonstration of readability');
+        art.title.should.equal('Chapter Demonstration of readability')
+        done();
+      });
+    });
+
+    it('minLength', function(done) {
+      read({
+        html: article,
+        betterTitle: 1000
+      }, function(err, art) {
+        should.not.exist(err);
+        console.log(art.title)
+        art.title.should.contain('GitHub');
+        done();
+      });
+    });
+
+    it('fn', function(done) {
+      read({
+        html: article,
+        betterTitle: function(t) {
+          return '[Github]' + t;
+        }
+      }, function(err, art) {
+        should.not.exist(err);
+        console.log(art.title)
+        art.title.should.contain('[Github]');
+        art.title.should.contain('readability');
         done();
       });
     });
